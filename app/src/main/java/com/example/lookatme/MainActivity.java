aa7cc162d40e9b28dd19455f2290e3efa11bcf12
bidhi212 <nidhikumaritreasure@gmail.com>
2023-08-08 23:21:35 +0530

    LookAtMe app

---
+++ app/src/main/java/com/example/lookatme/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.lookatme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.pd.lookatme.LookAtMe;
+
+
+public class MainActivity extends AppCompatActivity {
+    private LookAtMe lookAtMe;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lookAtMe = findViewById(R.id.lookme);
+        getSupportActionBar().hide();
+        lookAtMe.init(this);
+        lookAtMe.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videoplayback));
+        // lookAtMe.setVideoPath("http://website.com/video/mp4/62000/62792m.mp4"); to use video from a url
+
+        lookAtMe.start();
+        lookAtMe.setLookMe();
+    }
+}
