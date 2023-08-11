27f1d320214384b2da0aa5299e0610ae90b9e8b8
bidhi212 <nidhikumaritreasure@gmail.com>
2023-08-11 08:45:15 +0530

    added splash screen

--- app/src/main/java/com/example/lookatme/MainActivity.java
+++ app/src/main/java/com/example/lookatme/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lookatme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,12 +11,14 @@ import com.pd.lookatme.LookAtMe;
 
 public class MainActivity extends AppCompatActivity {
     private LookAtMe lookAtMe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        androidx.core.splashscreen.SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lookAtMe = findViewById(R.id.lookme);
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
         lookAtMe.init(this);
         lookAtMe.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videoplayback));
         // lookAtMe.setVideoPath("http://website.com/video/mp4/62000/62792m.mp4"); to use video from a url
